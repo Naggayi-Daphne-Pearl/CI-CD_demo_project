7c515a2fc8318122d7f55f59549a1c4a82aee622
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2022-06-05 22:06:00 +0300

    Failing tests

--- test/test.js
+++ test/test.js
@@ -1,3 +1,6 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const chai=require('chai');
 const chaiHttp=require('chai-http');
 chai.use(chaiHttp);
@@ -7,15 +10,29 @@ const logger=require('../Logger/logger');
 const server=require('../app');
 
 describe('register student',()=>{
- it('testing register',(done)=>{
+ it('Should run all api tasks ',(done)=>{
   chai.request('http://localhost:5000')
   .get('/register')
   .end((err,res)=>{
    res.should.have.status(200);
    expect(err).to.be.null;
+   res.body.should.have.property('name'); 
+   expect(attributes.name).to.eql('string');
    logger.info('register page rendered');
    done();
   })
- })
+ }); 
+
+ it('Should not run any api tasks ',(done)=>{
+    chai.request('http://localhost:5000')
+    .get('/register')
+    .end((err,res)=>{
+     res.should.have.status(404);
+     expect(err).to.be.null;
+     logger.error('test failed');
+     done();
+    })
+   });
+
 })
 
